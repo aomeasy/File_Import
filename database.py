287f10f472c2d0417233e9e141ffabeb5768d207
aomeasy <atcharaporn009@gmail.com>
2025-09-30 13:38:36 +0700

    Update database.py

--- database.py
+++ database.py
@@ -27,7 +27,7 @@ class DatabaseManager:
         except:
             self.connection_config = {
                 'host': os.getenv('DB_HOST'),
-                'port': int(os.getenv('DB_PORT', '3306')),
+                'port': int(os.getenv('DB_PORT', '33306')),
                 'database': os.getenv('DB_NAME'),
                 'user': os.getenv('DB_USER'),
                 'password': os.getenv('DB_PASSWORD'),
